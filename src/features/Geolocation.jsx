9e0b81e97b1f66034f4baad5dafc3234bf38625c
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-09-07 19:44:16 -0500

    Resolve fetch, set and use api data.

--- src/features/Geolocation.jsx
+++ src/features/Geolocation.jsx
@@ -1,64 +1,45 @@
 import React from 'react'
-
-/*function Geolocation() {
-    navigator.geolocation.getCurrentPosition(
-        function(position) {
-            const lat = position.coords.latitude
-            const lon = position.coords.longitude
-            fetch('https://weather-proxy.freecodecamp.rocks/api/current?lon=' + lon + '&lat=' + lat)
-            .then(response => response.json())
-            .then(
-                d => {
-                    const country = d.name + ', ' + d.sys.country
-                    const temperature = d.main.temp
-                    const main = d.weather[0].main
-                    const icon = d.weather[0].icon
-                    console.log(country, temperature, main, icon)
-                    
-                }
-            )
-        }
-    )
-    //Add button to change temperature
-}*/
-
 class Geolocation extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            api: 'https://weather-proxy.freecodecamp.rocks/api/current?',
-            country: "Country State"
+            country: "",
+            temperature: 0,
+            main: "",
+            icon: ""
         }
 
         this.success = this.success.bind(this)
     }
 
     success(position) {
-        this.setState({
-            api: 'https://weather-proxy.freecodecamp.rocks/api/current?lon=' + position.coords.longitude + '&lat' + position.coords.latitude
-        })
+        const api = 'https://weather-proxy.freecodecamp.rocks/api/current?lon=' + position.coords.longitude + '&lat=' + position.coords.latitude
+        fetch(api)
+        .then(response => response.json())
+        .then(
+            d => {
+                this.setState({
+                    country: d.name + ', ' + d.sys.country,
+                    temperature: d.main.temp,
+                    main: d.weather[0].main,
+                    icon: d.weather[0].icon
+                })
+            }
+        )
+    }
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(this.success)
     }
 
     render() {
-        navigator.geolocation.getCurrentPosition(this.success)
-        /*fetch(this.state.api)
-            .then(response => response.json())
-            .then(
-                d => {
-                    this.setState({
-                        country: d.name + ', ' + d.sys.country
-                    })
-                    const country = d.name + ', ' + d.sys.country
-                    const temperature = d.main.temp
-                    const main = d.weather[0].main
-                    const icon = d.weather[0].icon
-                    console.log(country, temperature, main, icon)
-                }
-            )*/
         return(
             <div>
-                <p>{this.state.api}</p>
-                <p>{this.state.country}</p>
+                <h1>{this.state.country}</h1>
+                {/*<Temperature temperature={this.state.temperature}/>*/}
+                {/*<h2>{this.state.temperature == 0 ? "" : this.state.temperature}</h2>*/}
+                <h3>{this.state.main}</h3>
+                <img src={this.state.icon}/>
             </div>
         )
     }
